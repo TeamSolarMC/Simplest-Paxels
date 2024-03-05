1981eebfcdbd903c13de615eeb52dc76bae213e8
Mike <misamaloman@gmail.com>
2024-03-05 22:58:22 +0200

    v1.0.0 Added: 1) Eng and Ru lang. 2) Textures for paxels.

--- src/main/java/net/indevo/simplest_paxels/datagen/ModItemModelProvider.java
+++ src/main/java/net/indevo/simplest_paxels/datagen/ModItemModelProvider.java
@@ -28,57 +28,9 @@ public class ModItemModelProvider extends ItemModelProvider {
         handheldItem(ModItems.NETHERITE_PAXEL);
     }
 
-    private ItemModelBuilder complexBlock(Block block) {
-        return withExistingParent(ForgeRegistries.BLOCKS.getKey(block).getPath(), new ResourceLocation(SimplestPaxels.MOD_ID,
-                "block/" + ForgeRegistries.BLOCKS.getKey(block).getPath()));
-    }
-
-    private ItemModelBuilder saplingItem(RegistryObject<Block> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(SimplestPaxels.MOD_ID,"block/" + item.getId().getPath()));
-    }
-
-    public void evenSimplerBlockItem(RegistryObject<Block> block) {
-        this.withExistingParent(SimplestPaxels.MOD_ID + ":" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
-                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath()));
-    }
-
-    public void trapdoorItem(RegistryObject<Block> block) {
-        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(),
-                modLoc("block/" + ForgeRegistries.BLOCKS.getKey(block.get()).getPath() + "_bottom"));
-    }
-
-    public void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
-        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
-                .texture("texture", new ResourceLocation(SimplestPaxels.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
-    }
-
-    public void buttomItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
-        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
-                .texture("texture", new ResourceLocation(SimplestPaxels.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
-    }
-
-    public void wallItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
-        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/wall_inventory"))
-                .texture("wall", new ResourceLocation(SimplestPaxels.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
-    }
-
     private ItemModelBuilder handheldItem(RegistryObject<Item> item) {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/handheld")).texture("layer0",
                 new ResourceLocation(SimplestPaxels.MOD_ID, "item/" + item.getId().getPath()));
-    }
-
-    private ItemModelBuilder simpleBlockItem(RegistryObject<Block> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(SimplestPaxels.MOD_ID, "item/" + item.getId().getPath()));
-    }
-
-    private ItemModelBuilder simpleBlockItemBlockTexture(RegistryObject<Block> item) {
-        return withExistingParent(item.getId().getPath(),
-                new ResourceLocation("item/generated")).texture("layer0",
-                new ResourceLocation(SimplestPaxels.MOD_ID, "block/" + item.getId().getPath()));
     }
 }
--- src/main/java/net/indevo/simplest_paxels/item/ModItems.java
+++ src/main/java/net/indevo/simplest_paxels/item/ModItems.java
@@ -13,17 +13,17 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, SimplestPaxels.MOD_ID);
 
     public static final RegistryObject<Item> WOOD_PAXEL = ITEMS.register("wood_paxel",
-            () -> new PaxelItem(ModToolTiers.WOOD, 5, -3.4F, new Item.Properties().durability(88)));
+            () -> new PaxelItem(ModToolTiers.WOOD, 2.5F, -3.4F, new Item.Properties().durability(88)));
     public static final RegistryObject<Item> STONE_PAXEL = ITEMS.register("stone_paxel",
-            () -> new PaxelItem(ModToolTiers.STONE, 5, -3.4F, new Item.Properties().durability(196)));
+            () -> new PaxelItem(ModToolTiers.STONE, 2.5F, -3.4F, new Item.Properties().durability(196)));
     public static final RegistryObject<Item> IRON_PAXEL = ITEMS.register("iron_paxel",
-            () -> new PaxelItem(ModToolTiers.IRON, 5, -3.4F, new Item.Properties().durability(375)));
+            () -> new PaxelItem(ModToolTiers.IRON, 2.5F, -3.4F, new Item.Properties().durability(375)));
     public static final RegistryObject<Item> GOLD_PAXEL = ITEMS.register("gold_paxel",
-            () -> new PaxelItem(ModToolTiers.GOLD, 5, -3.4F, new Item.Properties().durability(48)));
+            () -> new PaxelItem(ModToolTiers.GOLD, 2.5F, -3.4F, new Item.Properties().durability(48)));
     public static final RegistryObject<Item> DIAMOND_PAXEL = ITEMS.register("diamond_paxel",
-            () -> new PaxelItem(ModToolTiers.DIAMOND, 5, -3.4F, new Item.Properties().durability(2341)));
+            () -> new PaxelItem(ModToolTiers.DIAMOND, 2.5F, -3.4F, new Item.Properties().durability(2341)));
     public static final RegistryObject<Item> NETHERITE_PAXEL = ITEMS.register("netherite_paxel",
-            () -> new PaxelItem(ModToolTiers.NETHERITE, 5, -3.4F, new Item.Properties().durability(3046)));
+            () -> new PaxelItem(ModToolTiers.NETHERITE, 2.5F, -3.4F, new Item.Properties().durability(3046)));
 
 
 
